06a6e30eee224796a3d802219e541d6073e4bd72
Dharanidharan2509 <tharanidhanam@gmail.com>
2023-04-05 11:42:27 +0530

    Initial commit

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ function speechToText (options, callback) {
             }
         }
     }
+
+    return { startListening, stopListening }
 }
 
 function startListening () {
@@ -30,36 +32,4 @@ function stopListening () {
     recognition.stop();
 }
 
-// class speechToText {
-//     constructor (options, callback) {
-//         this.recognition = new webkitSpeechRecognition();
-//         this.recognition.interimResults = options.interimResults
-//         this.recognition.lang = options.lang
-//         this.recognition.continuous = options.continuous
-
-//         let final_transcript = "";
-
-//         this.recognition.onresult = (event) => {
-//             let interim_transcript = "";
-//             for (let i = event.resultIndex; i < event.results.length; ++i) {
-//                 if (event.results[i].isFinal) {
-//                     final_transcript += event.results[i][0].transcript;
-//                     callback(final_transcript)
-//                 } else {
-//                     interim_transcript += event.results[i][0].transcript;
-//                     callback(interim_transcript)
-//                 }
-//             }
-//         }
-//     }
-
-//     startListening () {
-//         this.recognition.start();
-//     }
-    
-//     stopListening () {
-//         this.recognition.stop();
-//     }
-// }
-
-export { speechToText, startListening, stopListening }
+export { speechToText }
