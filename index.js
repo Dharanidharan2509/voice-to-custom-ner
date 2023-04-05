be6af1643665f19a26954f62e8b840991d0475be
Dharanidharan2509 <tharanidhanam@gmail.com>
2023-04-05 10:46:04 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,33 +1,64 @@
-class speechToText {
-    constructor (options, callback) {
-        this.recognition = new webkitSpeechRecognition();
-        this.recognition.interimResults = options.interimResults
-        this.recognition.lang = options.lang
-        this.recognition.continuous = options.continuous
+const recognition = new webkitSpeechRecognition();
 
-        let final_transcript = "";
+function speechToText (options, callback) {
+    recognition.interimResults = options.interimResults
+    recognition.lang = options.lang
+    recognition.continuous = options.continuous
 
-        this.recognition.onresult = (event) => {
-            let interim_transcript = "";
-            for (let i = event.resultIndex; i < event.results.length; ++i) {
-                if (event.results[i].isFinal) {
-                    final_transcript += event.results[i][0].transcript;
-                    callback(final_transcript)
-                } else {
-                    interim_transcript += event.results[i][0].transcript;
-                    callback(interim_transcript)
-                }
+    let final_transcript = "";
+
+    recognition.onresult = (event) => {
+        let interim_transcript = "";
+        for (let i = event.resultIndex; i < event.results.length; ++i) {
+            if (event.results[i].isFinal) {
+                final_transcript += event.results[i][0].transcript;
+                callback(final_transcript)
+            } else {
+                interim_transcript += event.results[i][0].transcript;
+                callback(interim_transcript)
             }
         }
     }
-
-    startListening () {
-        this.recognition.start();
-    }
-    
-    stopListening () {
-        this.recognition.stop();
-    }
 }
 
-export { speechToText }
+function startListening () {
+    recognition.start();
+}
+
+function stopListening () {
+    recognition.stop();
+}
+
+// class speechToText {
+//     constructor (options, callback) {
+//         this.recognition = new webkitSpeechRecognition();
+//         this.recognition.interimResults = options.interimResults
+//         this.recognition.lang = options.lang
+//         this.recognition.continuous = options.continuous
+
+//         let final_transcript = "";
+
+//         this.recognition.onresult = (event) => {
+//             let interim_transcript = "";
+//             for (let i = event.resultIndex; i < event.results.length; ++i) {
+//                 if (event.results[i].isFinal) {
+//                     final_transcript += event.results[i][0].transcript;
+//                     callback(final_transcript)
+//                 } else {
+//                     interim_transcript += event.results[i][0].transcript;
+//                     callback(interim_transcript)
+//                 }
+//             }
+//         }
+//     }
+
+//     startListening () {
+//         this.recognition.start();
+//     }
+    
+//     stopListening () {
+//         this.recognition.stop();
+//     }
+// }
+
+export { speechToText, startListening, stopListening }
